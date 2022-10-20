818347869864b8f565b236ad4e74983a69130318
yuuhi <hainm112123@gmail.com>
2022-10-20 10:09:30 +0700

    'chatbox-layout'

--- middlewares/userData.middleware.js
+++ middlewares/userData.middleware.js
@@ -4,6 +4,8 @@ module.exports = {
   checkData: async function(req, res, next) {
     var user = await User.findById(req.signedCookies.userId);
     res.locals.user = user;
+    var users = await User.find();
+    res.locals.users = users;
     next();
   }
 }
